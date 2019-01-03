3d35aae0c99cb2d8dcebdf6601b5ac5df5e518e3
indhifarhandika <indhifarhandika@gmail.com>
2019-01-03 20:36:32 +0700

    Update Server & Add gitignore

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ __email__ = "indhifarhandika@gmail.com"
 from project import app
 
 if __name__ == '__main__':
-    app.run(host="192.168.1.26", port=8000, debug=True)
+    app.run(host="localhost", port=8000, debug=True)
